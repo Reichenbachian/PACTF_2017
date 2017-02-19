8e9a5136ea880c7cf4030bc921106e2f7f8b11bb
Reichenbachian <alex@reichenbach.org>
2017-02-19 13:28:00 -0500

    Added Graders and yamls

--- Binary/Problem1/README.md
+++ Binary/Problem1/README.md
@@ -1,2 +1,1 @@
 Simple problem. Executing does nothing, but there is a flag in the executable.
-Flag is flag_1297831859
---
+++ Binary/Problem1/grader.py
@@ -0,0 +1,5 @@
+def grade(key, submission):
+    if submission == 'flag_1297831859':
+        return True, 'Well found good sir'
+    else:
+        return False, 'What the heckeroni?'
