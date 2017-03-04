6c8b032d4fff7ce1eea1345e19e33eb93bd0de34
Reichenbachian <alex@reichenbach.org>
2017-03-04 15:50:08 -0500

    Adding a binary Problem

---
+++ Binary/Problem3/grader.py
@@ -0,0 +1,5 @@
+def grade(key, submission):
+    if submission == 'abcx!ilgdn':
+        return True, 'You are a true magician'
+    else:
+        return False, 'It might work if you run it again!'
