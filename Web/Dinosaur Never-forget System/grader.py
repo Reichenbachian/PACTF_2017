657ae6ac2227edaeb0b4682fc2746ef4b354627c
Miles McCain <milesrmcc@gmail.com>
2017-03-05 10:43:48 -0500

    Add 'Dinosaur Neverforget System' Problem

---
+++ Web/Dinosaur Never-forget System/grader.py
@@ -0,0 +1,5 @@
+def grade(key, submission):
+    if submission == 'dinosaurs_must_stay_informed':
+        return True, 'And so must you. Onward!'
+    else:
+        return False, 'Whois stopping you?'
