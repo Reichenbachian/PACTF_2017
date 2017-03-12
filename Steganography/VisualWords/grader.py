c93a98c0c180e69549c3e3636329f3c808a7f7f1
Reichenbachian <alex@reichenbach.org>
2017-03-12 17:00:29 +0100

    Adding VisualWords First commit

---
+++ Steganography/VisualWords/grader.py
@@ -0,0 +1,5 @@
+def grade(key, submission):
+    if submission == 'flag_HidingSomeFunStuffInThisImage!!':
+        return True, 'Visual Words!'
+    else:
+        return False, 'A bit of synethesia happening here...'
