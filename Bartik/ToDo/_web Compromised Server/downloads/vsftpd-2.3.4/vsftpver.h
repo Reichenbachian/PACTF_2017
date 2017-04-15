77b67e4c18f203789cb708fdc46e8e1d94a2a746
Miles McCain <milesrmcc@gmail.com>
2017-04-14 20:00:03 -0400

    remove web compromised server

--- Bartik/ToDo/_web Compromised Server/downloads/vsftpd-2.3.4/vsftpver.h
+++
@@ -1,7 +0,0 @@
-#ifndef VSF_VERSION_H
-#define VSF_VERSION_H
-
-#define VSF_VERSION "2.3.4"
-
-#endif /* VSF_VERSION_H */
-
--- Bartik/ToDo/_web Compromised Server/grader.py
+++
@@ -1,5 +0,0 @@
-def grade(arg, key):
-	if "you've_got_to_watch_out_for_backdoored_software" in key:
-		return True, "Good job"
-	else:
-		return False, "Keep searching"
