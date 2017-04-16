eea4b337206a801279bbf87c5011ffa6f912ed6f
Yatharth Agarwal <yatharth999@gmail.com>
2017-04-16 00:24:18 -0400

    Convert Caesar cipher to dynamic problem

---
+++ bartik/5 - Et tu Brute/generator.py
@@ -0,0 +1,30 @@
+import base64
+
+DESCRIPTION = """
+I found a message from Julius. Can you get the flag?
+Huk aopz pz aol mshn: {}
+"""
+
+HINT = """
+Look up a Caesar cipher
+"""
+
+ROT_LEVEL = 7
+
+
+def flag(key):
+    dynamic_bit = str(base64.encodebytes(bytes(str(key), encoding='utf8')), encoding='utf8')[:6]
+    return "veni_vidi_vici_{}".format(dynamic_bit)
+
+
+def rot_factory(n):
+    from string import ascii_lowercase as lc, ascii_uppercase as uc
+    lookup = str.maketrans(lc + uc, lc[n:] + lc[:n] + uc[n:] + uc[:n])
+    return lambda s: s.translate(lookup)
+
+
+rot = rot_factory(ROT_LEVEL)
+
+
+def generate(key):
+    return DESCRIPTION.format(rot(flag(key))).replace("_", "\_"), HINT
--- bartik/5 - Et tu Brute/grader.py
+++ bartik/5 - Et tu Brute/grader.py
@@ -1,5 +1,13 @@
+import base64
+
+
+def flag(key):
+    dynamic_bit = str(base64.encodebytes(bytes(str(key), encoding='utf8')), encoding='utf8')[:6]
+    return "veni_vidi_vici_{}".format(dynamic_bit)
+
+
 def grade(key, submission):
-    if submission == 'veni_vidi_vici':
+    if submission == flag(key):
         return True, "Nice job!"
     else:
         return False, 'Keep shifting'
