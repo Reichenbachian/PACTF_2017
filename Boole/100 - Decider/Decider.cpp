b2a7f3331725db02784d846459573016964a03fe
Reichenbachian <alex@reichenbach.org>
2017-04-22 23:21:01 -0400

    Changing decider

--- Boole/100 - Decider/Decider.cpp
+++ Boole/100 - Decider/Decider.cpp
@@ -6,15 +6,30 @@ using namespace std;
 	
 int main()
 {
-	string flag = "lulz - Good Luck!";
+	string flag = "youH4veBe3nChA11engedAndSucc33d3d!Nice!";
 	string shorterFlag="";
-	for (int i = 0; i < flag.length()/2; i++) {
-		shorterFlag += to_string((int)(flag[2*i+1]+flag[2*i])) + ",";
+	for (int i = 0; i < flag.length(); i++) {
+		if(i == flag.length() - 1) {
+			shorterFlag += to_string((int)(flag[i])) + ",";
+		} else {
+			shorterFlag += to_string((int)(flag[i+1]+flag[i])) + ",";
+		}
 	}
+	cout << shorterFlag << endl;
 	for (int i = 0; i < shorterFlag.length(); i++) {
 		shorterFlag[i] = shorterFlag[i]^'a';
 	}
-
 	cout << shorterFlag << endl;
+	srand (time(NULL));
+	int iSecret = rand() % 100 + 1;
+	int i = 0;
+	cout << iSecret << endl;
+	while (i <= iSecret) {
+		for (int i = 0; i < shorterFlag.length(); i++) {
+			shorterFlag[i] = shorterFlag[i]^iSecret;
+		}
+		i++;
+	}
+	cout << "TEST: " << shorterFlag << endl;
 	return 0;
 }
--- Boole/100 - Decider/grader.py
+++ Boole/100 - Decider/grader.py
@@ -1,5 +1,5 @@
 def grade(key, flag):
-    if flag == "ThisIsTheWonderFulDecIder,I'mGONNaMakEIt1ongJusttOPISSUOFFDOE!":
+    if flag == "youH4veBe3nChA11engedAndSucc33d3d!Nice!":
         return True, "Damn. Impressive."
     else:
         return False, "Try again!"
