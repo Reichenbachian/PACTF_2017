d718454349e4f6302975c6b1fdd3003e992e49cd
Reichenbachian <alex@reichenbach.org>
2017-04-22 22:53:41 -0400

    Updating problems for tomorrow

---
+++ Boole/40 - KeyGen/grader.py
@@ -0,0 +1,5 @@
+def grade(key, submission):
+    if submission == 'm3_LIKES_dat_DRMz!':
+        return True, 'Nice crack!'
+    else:
+        return False, 'Let\'s just imagine I\'m a button masher...'
