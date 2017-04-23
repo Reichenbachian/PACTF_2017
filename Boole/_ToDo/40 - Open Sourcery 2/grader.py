95c9c28817936b63c0dbe4b6ae701f13f305b191
Tony Zhaocheng Tan <tony@tonytan.io>
2017-04-22 23:42:54 -0400

    fix open sourcery 2

--- Boole/_ToDo/40 - Open Sourcery 2/grader.py
+++ Boole/_ToDo/40 - Open Sourcery 2/grader.py
@@ -1,5 +1,5 @@
 def grade(key, flag):
-    if "4boxdpa21ogonzkcrs9p" in flag:
-        return True, "HSTS is a good security measure. If you have a website, consider using it."
+    if "impactfestival.be" in flag:
+        return True, "HSTS is a good security measure. If you have a website, consider using it. By the way, we are not affiliated with that website."
     else:
         return False, "This is not it. Keep looking."
