efdd63efaa030f48e1e04ec4a48cd6d9fe0911a0
Reichenbachian <alex@reichenbach.org>
2017-04-25 07:29:23 -0400

    Fixing decider

--- boole/100 - Decider/Decider.cpp
+++ boole/100 - Decider/Decider.cpp
@@ -6,7 +6,7 @@ using namespace std;
 	
 int main()
 {
-	string flag = "youH4veBe3nChA11engedAndSucc33d3d!Nice!";
+	string flag = "Good Luck!";
 	string shorterFlag="";
 	for (int i = 0; i < flag.length(); i++) {
 		if(i == flag.length() - 1) {
@@ -15,21 +15,18 @@ int main()
 			shorterFlag += to_string((int)(flag[i+1]+flag[i])) + ",";
 		}
 	}
-	cout << shorterFlag << endl;
 	for (int i = 0; i < shorterFlag.length(); i++) {
 		shorterFlag[i] = shorterFlag[i]^'a';
 	}
-	cout << shorterFlag << endl;
 	srand (time(NULL));
-	int iSecret = rand() % 100 + 1;
+	int iSecret = rand() % 50 + 1;
 	int i = 0;
-	cout << iSecret << endl;
 	while (i <= iSecret) {
 		for (int i = 0; i < shorterFlag.length(); i++) {
 			shorterFlag[i] = shorterFlag[i]^iSecret;
 		}
 		i++;
 	}
-	cout << "TEST: " << shorterFlag << endl;
+	cout << "Encrypted To: " << shorterFlag << endl;
 	return 0;
 }
--- boole/100 - Decider/grader.py
+++ boole/100 - Decider/grader.py
@@ -1,5 +1,5 @@
 def grade(key, flag):
-    if flag == "youH4veBe3nChA11engedAndSucc33d3d!Nice!":
+    if flag == "flag_ziecEfBI9ggwZyoImhyv":
         return True, "Damn. Impressive."
     else:
         return False, "Try again!"
