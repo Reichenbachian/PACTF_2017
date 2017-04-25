793642b7be49bfdd04143636c353ac509973836e
Tony Z. Tan via PACTF Server <tony@pactf.com>
2017-04-25 22:26:24 +0000

    Changing drm grader

--- boole/40 - KeyGen/grader.py
+++ boole/40 - KeyGen/grader.py
@@ -1,5 +1,5 @@
 def grade(key, submission):
-    if submission == 'm3_LIKES_dat_DRMz!':
+    if submission == 'm3_LIKES_dat_DRM':
         return True, 'Nice crack!'
     else:
         return False, 'Let\'s just imagine I\'m a button masher...'
